66baeaf232cb45a5e44d51982876b9cd296e45b2
Plinio Balduino <pbalduino+github@gmail.com>
2014-01-07 11:48:46 -0200

    Created library skeleton

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to limon-migration
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/limon_migration/core_test.clj
@@ -0,0 +1,7 @@
+(ns limon-migration.core-test
+  (:require [clojure.test :refer :all]
+            [limon-migration.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
